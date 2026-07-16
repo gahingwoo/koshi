f27adb0d28680f1339fb25152ae7952ac9ec5a78
Nika Krasnova <nika@nikableh.moe>
2026-07-17 01:02:52 +0200

    build(flake): restructure around flake-utils and fix sandboxed tests

--- flake.nix
+++ flake.nix
@@ -1,76 +1,92 @@
 {
-  description = "Koshi - a GTK4 + libadwaita application";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems
-        (system: f nixpkgs.legacyPackages.${system});
-    in
+  outputs =
     {
-      packages = forAllSystems (pkgs: {
-        default = pkgs.rustPlatform.buildRustPackage {
-          pname = "koshi";
-          version = "0.1.0";
-          src = self;
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
 
-          cargoLock.lockFile = ./Cargo.lock;
+        packages.default =
+          let
+            pname = "koshi";
+            version = "0.1.0";
+            src = self;
+          in
+          pkgs.rustPlatform.buildRustPackage {
+            inherit pname version src;
 
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            wrapGAppsHook4
-          ];
+            cargoLock.lockFile = ./Cargo.lock;
 
-          buildInputs = with pkgs; [
-            glib
-            glib-networking
-            gtk4
-            libadwaita
-            libsoup_3
-          ];
+            # The send tests create scratch files under glib's user runtime
+            # dir; in the build sandbox XDG_RUNTIME_DIR is unset and glib's
+            # fallback (under HOME) is unwritable.
+            preCheck = ''
+              export XDG_RUNTIME_DIR=$(mktemp -d)
+            '';
 
-          # Install the desktop entry and hicolor app icons so the shell and
-          # launchers can find Koshi's icon by its application ID.
-          postInstall = ''
-            install -Dm644 data/moe.nikableh.Koshi.desktop \
-              $out/share/applications/moe.nikableh.Koshi.desktop
-            install -Dm644 data/icons/scalable/apps/moe.nikableh.Koshi.svg \
-              $out/share/icons/hicolor/scalable/apps/moe.nikableh.Koshi.svg
-            install -Dm644 data/icons/scalable/apps/moe.nikableh.Koshi.Devel.svg \
-              $out/share/icons/hicolor/scalable/apps/moe.nikableh.Koshi.Devel.svg
-            install -Dm644 data/icons/symbolic/apps/moe.nikableh.Koshi-symbolic.svg \
-              $out/share/icons/hicolor/symbolic/apps/moe.nikableh.Koshi-symbolic.svg
-          '';
+            nativeBuildInputs = [
+              pkgs.pkg-config
+              pkgs.wrapGAppsHook4
+            ];
 
-          meta = {
-            description = "Koshi";
-            mainProgram = "koshi";
-            platforms = pkgs.lib.platforms.linux;
+            buildInputs = [
+              pkgs.glib
+              pkgs.glib-networking
+              pkgs.gtk4
+              pkgs.libadwaita
+              pkgs.libsoup_3
+            ];
+
+            # Install the desktop entry and hicolor app icons so the shell and
+            # launchers can find Koshi's icon by its application ID.
+            postInstall = ''
+              install -Dm644 data/moe.nikableh.Koshi.desktop \
+                $out/share/applications/moe.nikableh.Koshi.desktop
+              install -Dm644 data/icons/scalable/apps/moe.nikableh.Koshi.svg \
+                $out/share/icons/hicolor/scalable/apps/moe.nikableh.Koshi.svg
+              install -Dm644 data/icons/scalable/apps/moe.nikableh.Koshi.Devel.svg \
+                $out/share/icons/hicolor/scalable/apps/moe.nikableh.Koshi.Devel.svg
+              install -Dm644 data/icons/symbolic/apps/moe.nikableh.Koshi-symbolic.svg \
+                $out/share/icons/hicolor/symbolic/apps/moe.nikableh.Koshi-symbolic.svg
+            '';
+
+            meta = {
+              description = "Read and reply to kernel mailing lists";
+              homepage = "https://github.com/nikableh/koshi";
+              license = pkgs.lib.licenses.gpl3Only;
+              mainProgram = "koshi";
+              platforms = pkgs.lib.platforms.linux;
+            };
           };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/koshi";
         };
-      });
 
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-            rust-analyzer
-            rustfmt
-            clippy
-            pkg-config
-          ];
-
-          buildInputs = with pkgs; [
-            glib
-            glib-networking
-            gtk4
-            libadwaita
-            libsoup_3
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          buildInputs = [
+            pkgs.bashInteractive
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+            pkgs.clippy
+            # Enable services.flatpak.enable = true; in configuration.nix, it
+            # won't work without it.
+            pkgs.flatpak
+            pkgs.flatpak-builder
           ];
 
           # `cargo run` bypasses wrapGAppsHook4, so expose GSettings schemas
@@ -81,6 +97,6 @@
             export GIO_EXTRA_MODULES=${pkgs.glib-networking}/lib/gio/modules''${GIO_EXTRA_MODULES:+:$GIO_EXTRA_MODULES}
           '';
         };
-      });
-    };
+      }
+    );
 }

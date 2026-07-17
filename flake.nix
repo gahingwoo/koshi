e4a24bdea32a3a84af7d75af886a78faf0edb133
Nika Krasnova <nika@nikableh.moe>
2026-07-17 14:33:58 +0200

    chore: release v0.2.1

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         packages.default =
           let
             pname = "koshi";
-            version = "0.2.0";
+            version = "0.2.1";
             src = self;
           in
           pkgs.rustPlatform.buildRustPackage {
